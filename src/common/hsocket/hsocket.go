9a56b589d0f68add4af6c4513771834e3d83a380
HuYi16 <huyihy@yeah.net>
2019-07-08 23:09:27 +0800

    prepare to socket

--- src/common/hsocket/hsocket.go
+++ src/common/hsocket/hsocket.go
@@ -2,7 +2,15 @@ package hsocket
 
 import(
    L "/common/hlog"
+   "net"
+   "sync"
+   "time"
+   "unsafe"
 )
+
+type Read func() bool //read fun callback
+
+type DisCon func() //disconnect fun callback
 
 func init(){
    L.W("inti socket package",L.Level_Normal) 
